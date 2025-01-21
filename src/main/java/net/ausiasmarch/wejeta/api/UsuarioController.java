0f4f9f1a841e31de4ceac07c64a5b8751c0951a3
EloyML05 <eloymiotalopez3@gmail.com>
2025-01-21 03:26:58 +0100

    añadido el metodo de delete

--- src/main/java/net/ausiasmarch/wejeta/api/UsuarioController.java
+++ src/main/java/net/ausiasmarch/wejeta/api/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,10 @@ public class UsuarioController {
     @GetMapping("")
     public ResponseEntity<Page<UsuarioEntity>> getPage(Pageable oPageable) {
         return new ResponseEntity<Page<UsuarioEntity>>(oUsuarioService.getPage(oPageable), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable(value = "id") Long id) {
+        oUsuarioService.delete(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
     }
 }
